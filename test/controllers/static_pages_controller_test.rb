d9870338c312b0341a7bad000b34a959ed2f77ec
Thierry Meier <thierrymeier@gmail.com>
2016-01-03 03:40:32 +0000

    Add  and finish static pages

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,16 +4,19 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
+    assert_select "title", "Home | Better Days"
   end
 
   test "should get help" do
     get :help
     assert_response :success
+    assert_select "title", "Help | Better Days"
   end
 
   test "should get contact" do
     get :contact
     assert_response :success
+    assert_select "title", "Contact | Better Days"
   end
 
 end
