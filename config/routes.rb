0604409be211e27f19121d9faea9c9d55d082bc0
Thierry Meier <thierrymeier@gmail.com>
2016-01-03 04:04:18 +0000

    Add entries controller and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
-  root 'static_pages#home'
+  root 'entries#index'
+  get 'static_pages/home'
   get 'static_pages/help'
   get 'static_pages/contact'
+  get 'entries/new'
+  get 'entries/show'
+  get 'entries/edit'
+  
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
