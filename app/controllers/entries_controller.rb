098a5979dfe153a7a02d932ac60633c75407f9da
Thierry Meier <thierrymeier@gmail.com>
2016-01-03 14:14:26 +0000

    Add footer partial, add create action, add show action. Tie it all together.

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,14 +1,35 @@
 class EntriesController < ApplicationController
   def new
+    @entry = Entry.new
   end
 
   def index
     @entries = Entry.all
+    @entry = Entry.new
   end
 
   def show
+    @entry = Entry.find(params[:id])
   end
 
   def edit
   end
+  
+  def create
+    @entry = Entry.new(entry_params)
+    @entry.save
+    respond_to do |x|
+      
+      if @entry.save
+        x.html { redirect_to root_path, notice: 'Entry was successfully created.' }
+      else
+        x.html { render :new, notice: 'Entry could not be saved.' }
+      end
+    end
+  end
+  
+  private
+    def entry_params
+      params.require(:entry).permit(:content, :location)
+    end
 end
