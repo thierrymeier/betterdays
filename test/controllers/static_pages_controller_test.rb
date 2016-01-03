a79f4bee360d5cf572a172239ebf2a7fd744ad1f
Thierry Meier <thierrymeier@gmail.com>
2016-01-03 03:54:03 +0000

    Add guard and automate title generation

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,7 +4,7 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
-    assert_select "title", "Home | Better Days"
+    assert_select "title", "Better Days"
   end
 
   test "should get help" do
