99fd090e6740989cd6915e027719ae9c3c78e21b
Thierry Meier <thierrymeier@gmail.com>
2016-01-05 06:14:29 +0000

    Adapt search query to PostgreSQL

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -3,7 +3,8 @@ class Entry < ActiveRecord::Base
   
   
   def self.search(search)
-    where("content LIKE ? OR created_at LIKE ?", "%#{search}%", "%#{search}%") 
+    where("content LIKE :pat", :pat => "%#{search}%")
+    # where("content LIKE :pat OR to_char(created_at, 'YYYY month Month DD') LIKE :pat", :pat => "%#{search}%") <-- with timestamp search
   end
   
 end
