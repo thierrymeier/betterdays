8bf67823abc9b32f4b971ab3897a54167a0b3935
Thierry Meier <thierrymeier@gmail.com>
2016-01-05 03:47:28 +0000

    Implement faker and seed database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+24.times do |n|
+  content = Faker::Hipster.paragraphs(6).join("<br /> <br />")
+  location = Faker::Address.city
+  rating = Faker::Number.between(1, 10)
+  created_at = Time.now - (n+1)*86400
+  Entry.create!(content: content,
+                location: location,
+                rating: rating,
+                created_at: created_at)
+end
