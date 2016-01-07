0039b25e32fa0c71b0f8fb77edf910fd33fd271a
Thierry Meier <thierrymeier@gmail.com>
2016-01-07 03:50:14 +0000

    Improve validation models and implement tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,10 @@
 class User < ActiveRecord::Base
   has_many :entries, dependent: :destroy
+  before_save { self.email = email.downcase }
   has_secure_password
-  validates :email, presence: true, length: { maximum: 255 }
-  validates :password, length: { minimum: 6 }
-  validates_uniqueness_of :email
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, length: { maximum: 250 }, uniqueness: { 
+                    case_sensitive: false },format: { with: VALID_EMAIL_REGEX }
+  validates :password, presence: true, length: { minimum: 6 }
+  validates :password_confirmation, presence: true, length: { minimum: 6 }
 end
