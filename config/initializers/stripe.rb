c065f30f0f3811589c3247bbb9e1f97b03bd0254
Thierry Meier <thierrymeier@gmail.com>
2016-01-08 09:20:50 +0000

    Add Stripe

---
+++ config/initializers/stripe.rb
@@ -0,0 +1,6 @@
+Rails.configuration.stripe = {
+   :publishable_key => ENV['PUBLISHABLE_KEY'] ||= Rails.application.secrets.stripe_publishable_key,
+   :secret_key      => ENV['SECRET_KEY'] ||= Rails.application.secrets.stripe_secret_key
+}
+
+Stripe.api_key = Rails.application.secrets.stripe_secret_key
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :sessions
+  resources :charges
   resources :users, except: [:index, :show, :destroy]
   resources :entries do
     collection do
