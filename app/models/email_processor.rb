d5f4f8863c7e4d2e8103f25b9d9be88808f27206
Thierry Meier <thierrymeier@gmail.com>
2016-01-22 05:02:45 +0000

    Improve email process by creating an entry only if user has not journaled today.

--- app/models/email_processor.rb
+++ app/models/email_processor.rb
@@ -5,8 +5,10 @@ class EmailProcessor
   
   def process
     user = User.find_by_email(@email.from[:email])
-    user.entries.create!(
-      content: @email.body
-    )
+    user.entries.create!(content: @email.body) if !has_journaled_today?
+  end
+  
+  def has_journaled_today?
+    user.entries.first.try(:created_at).try(:strftime, '%d %B %Y') == Time.now.strftime("%d %B %Y")
   end
 end
