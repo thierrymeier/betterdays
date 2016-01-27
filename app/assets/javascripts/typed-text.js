7a0e9e2c631d50513fdd8ed664560e21ed1f63a7
Thierry Meier <thierrymeier@gmail.com>
2016-01-27 06:46:46 +0000

    Improve responsive design a lot

--- app/assets/javascripts/typed-text.js
+++ app/assets/javascripts/typed-text.js
@@ -1,7 +1,7 @@
 $(function(){
     $("h1.hero").typed({
       strings: ["Dear ^300 Diary ^500, "],
-      typeSpeed: 50,
+      typeSpeed: 30,
       showCursor: false
     });
 });
@@ -9,7 +9,7 @@ $(function(){
 $(function(){
     $("p.post").typed({
       strings: ["I am ^100 grateful for your <strong>clutter-free writing experience</strong>. ^300 Thank you for ^150 helping me journal everyday and ^200 keeping it <strong>safe</strong>."],
-      typeSpeed: 40,
+      typeSpeed: 25,
       showCursor: false,
       startDelay: 2500
     });
@@ -18,8 +18,8 @@ $(function(){
 $(function(){
     $("span.signature").typed({
       strings: ["Yours ^100 truly."],
-      typeSpeed: 50,
+      typeSpeed: 35,
       showCursor: false,
-      startDelay: 15000
+      startDelay: 13000
     });
 });
