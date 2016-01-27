19d3caa0702d12b9d28b5265bcce1fae1707eba8
Thierry Meier <thierrymeier@gmail.com>
2016-01-27 03:59:11 +0000

    Make including of specific JS more flexible

---
+++ app/assets/javascripts/typed-text.js
@@ -0,0 +1,25 @@
+$(function(){
+    $("h1.hero").typed({
+      strings: ["Dear ^300 Diary ^500, "],
+      typeSpeed: 50,
+      showCursor: false
+    });
+});
+
+$(function(){
+    $("p.post").typed({
+      strings: ["I am ^100 grateful for your <strong>clutter-free writing experience</strong>. ^300 Thank you for ^150 helping me journal everyday and ^200 keeping it <strong>safe</strong>."],
+      typeSpeed: 40,
+      showCursor: false,
+      startDelay: 2500
+    });
+});
+
+$(function(){
+    $("span.signature").typed({
+      strings: ["Yours ^100 truly."],
+      typeSpeed: 50,
+      showCursor: false,
+      startDelay: 15000
+    });
+});
