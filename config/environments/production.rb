f3cbf2d4b77fe2ed0f9e19ab522a0ba34de51084
Thierry Meier <thierrymeier@gmail.com>
2016-11-18 00:43:56 +0000

    Change host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,7 +60,7 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   host = 'quiet-reef-8046.herokuapp.com'
-  config.action_mailer.default_url_options = { host: 'www.betterdaysapp.com' }
+  config.action_mailer.default_url_options = { host: 'betterdaysapp.herokuapp.com' }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
     :port           => '587',
